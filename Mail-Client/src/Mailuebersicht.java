1d083fb65fd11de6978e7d41d699ff9b2d6a8793
FienchenBienchen94 <svorderwuelbecke@web.de>
2014-12-04 16:39:45 +0100

    lala

--- Mail-Client/src/Mailuebersicht.java
+++ Mail-Client/src/Mailuebersicht.java
@@ -23,7 +23,7 @@ public class Mailuebersicht extends Fenster {
 	private ArrayList<Mail> mails;
 	private File offlineMails;
 	private int aktuelleSeite;
-	Konto konto;
+	public Konto konto;
 	
 	public Mailuebersicht(Konto k) {
 		konto=k;
@@ -51,6 +51,7 @@ public class Mailuebersicht extends Fenster {
 		try {
 			Element root = new Element("konten");
 			Document doc = new Document(root);
+			Element e1=new Element(k.getName());
 			XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
 			out.output(doc,new FileOutputStream(offlineMails));
 		}catch(Exception e){
@@ -86,7 +87,7 @@ public class Mailuebersicht extends Fenster {
 
 			Mail tmp=mails.get(nummer);
 			Element e1=new Element("person");
-			root.addContent(e1);
+			root.getChild(konto.getName()).addContent(e1);
 			e1.addContent(new Element("adresse").addContent(tmp.getAdresse()));
 			e1.addContent(new Element("betreff").addContent(tmp.getBetreff()));
 			e1.addContent(new Element("nachricht").addContent(tmp.getNachricht()));
