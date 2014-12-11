17e9755c81b039954c8051d63da122d9485183fc
FienchenBienchen94 <svorderwuelbecke@web.de>
2014-12-11 13:37:57 +0100

    Hauptprogramm auch noch geändert

--- Mail-Client/src/Hauptprogramm.java
+++ Mail-Client/src/Hauptprogramm.java
@@ -1,18 +1,8 @@
 import java.util.Scanner;
 public class Hauptprogramm {
 	public static void main(String argv[]){
-		Scanner sc=new Scanner(System.in);
-		Fenster oben=new Fenster();
-		while(true){
-			System.out.println("Es gibt folgende Befehle zur Auswahl mit den zugehörigen Nummern:");
-			for(int i=0;i<oben.getAktuell().kommandoliste.size();i++){
-				System.out.println(oben.getAktuell().kommandoliste.get(i)+": "+i);
-			}
-			int befehl=sc.nextInt();
-			if(befehl<oben.getAktuell().kommandoliste.size()){
-
-
-		}
+		Startfenster st=new Startfenster();
+		st.auswaehlen();
 	}
 
 }
